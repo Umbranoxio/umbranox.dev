d320beac8bacd9fc975e26eaed0311ff28291e3f
Umbranox <itsumbra@protonmail.com>
2024-01-24 06:08:38 +1100

    navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,27 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
    theme: {
+      container: {
+         center: true,
+         padding: '2rem',
+         screens: {
+            '2xl': '1400px',
+         },
+      },
       extend: {
+         colors: {
+            syellow: '#FEDB47',
+         },
          fontFamily: {
             sans: ['Geist Mono', ...defaultTheme.fontFamily.sans],
+         },
+         borderRadius: {
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)',
+         },
+         transitionDuration: {
+            DEFAULT: '300ms',
          },
       },
    },
