8058116ed9c51449900f399cf7158aa77982e322
Umbranox <itsumbra@protonmail.com>
2024-01-24 09:34:27 +1100

    Revert "cf pages"
    This reverts commit 1a1ce8abb0d2c9d6f936d16096d7f8c37a25753d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })],
-  output: "server",
-  adapter: cloudflare()
+   integrations: [tailwind({ applyBaseStyles: false })],
 });
